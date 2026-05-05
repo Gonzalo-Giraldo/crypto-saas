8742e67aa1e52872be24521230fa327d489fb129
Gonzalo-Giraldo <gonzalogiraldo@yahoo.com>
2026-05-05 13:49:16 -0500

    merge alembic heads for binance exit protections

---
+++ alembic/versions/20260506_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads for exit protections
+
+Revision ID: 20260506_merge_heads
+Revises: 20260425_add_intent_financial_snapshot, 20260505_create_binance_exit_protections
+Create Date: 2026-05-06
+"""
+from alembic import op
+import sqlalchemy as sa
+
+revision = "20260506_merge_heads"
+down_revision = (
+    "20260425_add_intent_financial_snapshot",
+    "20260505_create_binance_exit_protections",
+)
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
